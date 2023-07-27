29d22e9e4d86210886830e8888d599dffd32afe3
archanak55201 <55021100+archanak55201@users.noreply.github.com>
2023-07-27 11:21:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function volume_sphere(e) {
 	e.preventDefault();
     let r=document.getElementById("radius").value;
-	let volume=(4*22*r*r*r)/(3*7);
+	let volume=((4*22*r*r*r)/(3*7)).toPrecision(7);
 	let vol=document.getElementById("volume");
 	vol.value=volume;
   
